d01e341cb17369eeb806c41c1a510761c68c0959
jiayiLin <linjiayi6@gmail.com>
2023-12-13 09:35:37 +0000

    fix

--- script_llava.sh
+++ script_llava.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-#$ -cwd
-#$ -j y
-#$ -pe smp 8      # Request 1 core
-#$ -l h_rt=20:0:0  # Request 1 hour runtime
-#$ -l h_vmem=11G   # Request 1GB RAM
-#$ -l gpu=1     # request 1 GPU
-##$ -l cluster=andrena # use the Andrena nodes
-
-
-source /data/DERI-Gong/jl010/envLISA2/bin/activate
-
-
 config=CHAMELEON_LLaVA1.5
 python main.py --config config/$config.yaml --visualization 
